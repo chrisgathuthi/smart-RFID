20b75eade4f535f51d02e88d30ab38272e2760af
Saint-Chris-7 <chrisgathuthi@gmail.com>
2022-10-11 01:41:53 +0300

    beta stage

--- security/generator/views.py
+++ security/generator/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.base import View
 from django.http import HttpResponse
-
+from .models import Student
 # Create your views here.
-def index(request,*args,** kwargs):
-    return HttpResponse("Hello world")
+
+
+class IndexTemplate(TemplateView):
+    template_name = "index.html"
+   
+
+#htmx views
+class FormData(View):
+    def get(self,request,*args,**kwargs):
+        data = request.GET.get("reg","")
+        qs = Student.objects.filter(reg_no__iexact=data)
+        return render(request,"results.html",{"qs":qs})
